a06ccd9aa5cebaceddcba1a81b7d1cffdaf9c40e
nikhilpj <nikhilpj98@gmail.com>
2023-06-26 13:45:57 +0530

    final changes

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,20 @@
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import { useDispatch,useSelector } from "react-redux";
+import { calculateTotal } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const {cartItems} = useSelector((store)=>store.cart)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(calculateTotal())
+  },[cartItems])
+  return <main>
+    <Navbar/>
+    <CartContainer/>
+  </main>;
 }
 export default App;
